3bcf3f22f3026a244fe434f88349503f9266a8e1
Ai Vong <aivong2@illinois.edu>
2016-05-01 18:33:22 -0500

    user auth

--- source_js/app.js
+++ source_js/app.js
@@ -21,35 +21,91 @@ var app = angular.module('lap', [
 
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
+        when('/', {
+          templateUrl: 'partials/home.html',
+          access: {restricted: true}
+        }).
+        when('/login', {
+          templateUrl: 'partials/user/login.html',
+          controller: 'loginController',
+          access: {restricted: false}
+        }).
+        when('/logout', {
+          controller: 'logoutController',
+          access: {restricted: true}
+        }).
+        when('/register', {
+          templateUrl: 'partials/user/register.html',
+          controller: 'registerController',
+          access: {restricted: false}
+        }).
+        when('/one', {
+          template: '<h1>This is page one!</h1>',
+          access: {restricted: true}
+        }).
+        when('/two', {
+          template: '<h1>This is page two!</h1>',
+          access: {restricted: false}
+        }).
         when('/posts/:id', {
             templateUrl: 'partials/post/details.html',
-            controller: 'PostIndividualController'
+            controller: 'PostIndividualController',
+            access: {restricted: true}
         }).
         when('/edit-post/:id', {
             templateUrl: 'partials/post/edit.html',
-            controller: 'PostIndividualController'
+            controller: 'PostIndividualController',
+            access: {restricted: true}
         }).
         when('/jobs/:id', {
             templateUrl: 'partials/job/details.html',
-            controller: 'JobsIndividualController'
+            controller: 'JobsIndividualController',
+            access: {restricted: true}
         }).
         when('/posts', {
             templateUrl: 'partials/post/list.html',
-            controller: 'PostsController'
+            controller: 'PostsController',
+            access: {restricted: true}
         }).
         when('/addpost', {
             templateUrl: 'partials/post/add.html',
-            controller: 'AddPostController'
+            controller: 'AddPostController',
+            access: {restricted: true}
         }).
         when('/jobs', {
             templateUrl: 'partials/job/list.html',
-            controller: 'JobListController'
+            controller: 'JobListController',
+            access: {restricted: true}
         }).
         when('/addjob', {
             templateUrl: 'partials/job/add.html',
-            controller: 'JobAddController'
+            controller: 'JobAddController',
+            access: {restricted: true}
         }).
         otherwise({
-            redirectTo: '/posts'
+            redirectTo: '/'
         });
 }]);
+
+app.run(function ($rootScope, $location, $route, AuthService) {
+  $rootScope.$on('$routeChangeStart',
+    function (event, next, current) {
+      console.log(next);
+      AuthService.getUserStatus()
+      .then(function(){
+        if (!AuthService.isLoggedIn() && next.access.restricted){
+          $location.path('/login');
+          $route.reload();
+        }
+      });
+  });
+});
+//
+// app.run(function ($rootScope, $location, $route, AuthService) {
+//   $rootScope.$on('$routeChangeStart',
+//     function (event, next, current) {
+//     if (AuthService.isLoggedIn() === false) {
+//       $location.path('/login');
+//     }
+//   });
+// });
--- source_js/services/user.js
+++ source_js/services/user.js
@@ -1,1 +1,106 @@
 var userServices = angular.module('user.services', []);
+var baseUrl = "http://localhost:4000/api";
+
+userServices.factory('AuthService', ['$q', '$timeout', '$http', function ($q, $timeout, $http) {
+
+      // create user variable
+      var user = null;
+
+      // return available functions for use in the controllers
+      return ({
+        isLoggedIn: function isLoggedIn() {
+                      if(user) {
+                          return true;
+                      } else {
+                          return false;
+                      }
+                    },
+        getUserStatus: function getUserStatus() {
+                        return $http.get(baseUrl + '/user/status', {withCredentials: true})
+                        // handle success
+                        .success(function (data) {
+                          if(data.status){
+                            user = true;
+                          } else {
+                            user = false;
+                          }
+                        })
+                        // handle error
+                        .error(function (data) {
+                          user = false;
+                        });
+                       },
+        login: function login(username, password) {
+
+                // create a new instance of deferred
+                var deferred = $q.defer();
+
+                // send a post request to the server
+                $http.post(baseUrl + '/user/login',
+                  {username: username, password: password})
+                  // handle success
+                  .success(function (data, status) {
+                    if(status === 200 && data.status){
+                      user = true;
+                      deferred.resolve();
+                    } else {
+                      user = false;
+                      deferred.reject();
+                    }
+                  })
+                  // handle error
+                  .error(function (data) {
+                    user = false;
+                    deferred.reject();
+                  });
+
+                // return promise object
+                return deferred.promise;
+              },
+        logout: function logout() {
+
+                  // create a new instance of deferred
+                  var deferred = $q.defer();
+
+                  // send a get request to the server
+                  $http.get(baseUrl + '/user/logout')
+                  // handle success
+                    .success(function (data) {
+                      user = false;
+                      deferred.resolve();
+                    })
+                    // handle error
+                    .error(function (data) {
+                      user = false;
+                      deferred.reject();
+                    });
+
+                  // return promise object
+                  return deferred.promise;
+                },
+        register: function register(username, password) {
+
+                    // create a new instance of deferred
+                    var deferred = $q.defer();
+
+                    // send a post request to the server
+                    $http.post(baseUrl + '/user/register',
+                      {username: username, password: password})
+                      // handle success
+                      .success(function (data, status) {
+                        if(status === 200 && data.status){
+                          deferred.resolve();
+                        } else {
+                          deferred.reject();
+                        }
+                      })
+                      // handle error
+                      .error(function (data) {
+                        deferred.reject();
+                      });
+
+                    // return promise object
+                    return deferred.promise;
+                  }
+      });
+}]);
